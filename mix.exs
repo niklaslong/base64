55f393aabb30263fd6960dfbb5b3fd3b19f4d736
nkls <niklas@long.ch>
2020-05-03 20:24:05 +0200

    add rustler and benchee to mix.exs deps

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Base64.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -21,8 +22,14 @@ defmodule Base64.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:rustler, github: "rusterlium/rustler", sparse: "rustler_mix", branch: "master"},
+      {:benchee, "~> 1.0"}
+    ]
+  end
+
+  defp aliases do
+    [
+      fmt: ["format", "cmd cargo fmt --manifest-path native/lars/Cargo.toml"]
     ]
   end
 end
