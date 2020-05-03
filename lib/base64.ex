7c00323c6f38f7774faba6b52d2ddcba9c3c30ce
nkls <niklas@long.ch>
2020-05-03 20:22:37 +0200

    initial commit

---
+++ lib/base64.ex
@@ -0,0 +1,18 @@
+defmodule Base64 do
+  @moduledoc """
+  Documentation for `Base64`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Base64.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/base64_test.exs
@@ -0,0 +1,8 @@
+defmodule Base64Test do
+  use ExUnit.Case
+  doctest Base64
+
+  test "greets the world" do
+    assert Base64.hello() == :world
+  end
+end
