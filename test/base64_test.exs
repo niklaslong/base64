a9386557304c862a61d48ff97528f55598a10e31
nkls <niklas@long.ch>
2020-05-13 11:05:37 +0200

    add tests

--- test/base64_test.exs
+++ test/base64_test.exs
@@ -44,6 +44,13 @@ defmodule Base64Test do
       end
     end
 
+    test "correctly encodes base64 (standard no padding)" do
+      check all s <- StreamData.string(:ascii),
+                b64_s = Base64.encode(s, :standard_no_pad) do
+        assert s == Base.decode64!(b64_s, padding: false)
+      end
+    end
+
     test "returns a valid string (url safe)" do
       check all s <- StreamData.string(:ascii),
                 b64_s = Base64.encode(s, :url_safe) do
@@ -51,10 +58,24 @@ defmodule Base64Test do
       end
     end
 
+    test "correctly encodes base64 (url safe)" do
+      check all s <- StreamData.string(:ascii),
+                b64_s = Base64.encode(s, :url_safe) do
+        assert s == Base.url_decode64!(b64_s)
+      end
+    end
+
     test "returns a valid string (url safe no padding)" do
       check all s <- StreamData.string(:ascii),
                 b64_s = Base64.encode(s, :url_safe_no_pad) do
         assert String.valid?(b64_s)
+      end
+    end
+
+    test "correctly encodes base64 (url safe no padding)" do
+      check all s <- StreamData.string(:ascii),
+                b64_s = Base64.encode(s, :url_safe_no_pad) do
+        assert s == Base.url_decode64!(b64_s, padding: false)
       end
     end
 
@@ -103,6 +124,20 @@ defmodule Base64Test do
       check all s <- StreamData.string(:ascii),
                 b64_s = Base.encode64(s) do
         assert s == Base64.decode(b64_s, :standard)
+      end
+    end
+
+    test "correctly decodes base64 (standard no padding)" do
+      check all s <- StreamData.string(:ascii),
+                b64_s = Base.encode64(s, padding: false) do
+        assert s == Base64.decode(b64_s, :standard_no_pad)
+      end
+    end
+
+    test "correctly decodes base64 (url safe)" do
+      check all s <- StreamData.string(:ascii),
+                b64_s = Base.url_encode64(s) do
+        assert s == Base64.decode(b64_s, :url_safe)
       end
     end
   end
